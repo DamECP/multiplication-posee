2f780f47a020c80fe926ac88d087bbc360f4c920
DamECP <damien.depaepe@gmail.com>
2025-02-05 21:18:47 +0100

    translation into english

--- multiplication_posee.py
+++ multiplication_posee.py
@@ -1,53 +1,54 @@
-def grand_nombre(n):
+def large_number(n):
     return f"{n:_}".replace("_", " ")
 
 
-def multiplication(n1: int, n2: int):
-    etages = []
-    # présenter la multiplication correctement : le plus grand au-dessus
-    terme1, terme2 = (n2, n1) if n2 > n1 else (n1, n2)
-    puissance = 1
+def multiply(n1: int, n2: int):
+    steps = []
+    # the biggest number is at the top
+    value1, value2 = (n2, n1) if n2 > n1 else (n1, n2)
+    power = 1
 
-    # résultats des étages
-    for i in reversed(str(terme2)):
-        result = int(i) * terme1 * puissance
-        etages.append(result)
-        # multiplie par 10 chaque étage
-        puissance *= 10
+    # steps results
+    for i in reversed(str(value2)):
+        result = int(i) * value1 * power
+        steps.append(result)
+        # *10 for each step
+        power *= 10
 
-    resultat = sum(etages)
+    final_result = sum(steps)
 
-    # mise en page
-    largeur = len(grand_nombre(resultat)) + 2
+    # design
+    width = len(large_number(final_result)) + 2
 
-    print(f"{grand_nombre(terme1):>{largeur}}")
-    print(f"x {grand_nombre(terme2):>{largeur -2}}")
-    print("─" * largeur)
+    print(f"{large_number(value1):>{width}}")
+    print(f"x {large_number(value2):>{width -2}}")
+    print("─" * width)
 
-    if len(etages) > 1:
-        print(f"{grand_nombre(etages[0]):>{largeur}}")
-        for i in etages[1:]:
-            print(f"+ {grand_nombre(i):>{largeur -2}}")
-        print("─" * largeur)
+    if len(steps) > 1:
+        print(f"{large_number(steps[0]):>{width}}")
+        for i in steps[1:]:
+            print(f"+ {large_number(i):>{width -2}}")
+        print("─" * width)
 
-    print(f"{grand_nombre(resultat):>{largeur}}")
+    print(f"{large_number(final_result):>{width}}")
 
 
-def demander_nombre(message):
+def ask_for_number(message):
     while True:
         try:
             return int(input(message))
         except ValueError:
-            print("Veuillez entrer un nombre valide.")
+            print("Integer only")
 
 
 while True:
-    n1 = demander_nombre("Première valeur (0 pour quitter) : ")
+    n1 = ask_for_number("First value (0 to quit) : ")
     if n1 == 0:
         break
-    n2 = demander_nombre("Deuxième valeur (0 pour quitter) : ")
+    n2 = ask_for_number("Second value (0 to quit) : ")
+    print()
     if n2 == 0:
         break
 
-    multiplication(n1, n2)
+    multiply(n1, n2)
     print()
